537bffb3b3b1062d637282754e92eb361aa18a56
Tr1pl3B <85549327+Tr1pl3B@users.noreply.github.com>
2024-02-01 19:03:18 +0100

    removed comments

--- Week1Exercise8.ino
+++ Week1Exercise8.ino
@@ -26,10 +26,6 @@ GCM<AES128> gcmAes128;
 GCM<AES192> gcmAes192;
 GCM<AES256> gcmAes256;
 
-// XTS<AES128> *xtsAes128;
-// XTS<AES192> *xtsAes192;
-// XTS<AES256> *xtsAes256;
-
 void runExperiment(Cipher *cipher, uint8_t size) {
     uint8_t plaintext[16]; // Assuming 128-bit block size
     uint8_t key[32]; // Maximum key size for AES-256
@@ -116,19 +112,6 @@ void setup() {
     Serial.println("AES256");
     runExperiment(&gcmAes256,32);
     Serial.println();
-
-    // Serial.println("------------------------------------------------");
-    // Serial.println("XTS");
-    // Serial.println("-----");
-    // Serial.println("AES128");
-    // runExperiment(&xtsAes128,16);
-    // Serial.println("AES192");
-    // runExperiment(&xtsAes192,24);
-    // Serial.println("AES256");
-    // runExperiment(&xtsAes256,32);
-    // Serial.println();
-
-    //CTR, EAX, GCM, XTS
 }
 
 void loop() {
