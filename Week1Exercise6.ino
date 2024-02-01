3ed479720249a33fc364f88b4a2fbf581a5feb6f
Tr1pl3B <85549327+Tr1pl3B@users.noreply.github.com>
2024-02-01 18:02:37 +0100

    Create Week1Exercise6.ino

---
+++ Week1Exercise6.ino
@@ -0,0 +1,79 @@
+#include <AES.h>
+#include <Arduino.h>
+#include <Crypto.h>
+#include <CTR.h>
+#include <GCM.h>
+
+#define NUM_RUNS 100
+
+void generateRandomPlaintext(uint8_t* plaintext, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        plaintext[i] = random(256);
+    }
+}
+
+AES128 aes128;
+AES192 aes192;
+AES256 aes256;
+
+void runExperiment(BlockCipher *cipher) {
+    uint8_t plaintext[16]; // Assuming 128-bit block size
+    uint8_t key[32]; // Maximum key size for AES-256
+    uint8_t iv[16]; // Initialization Vector
+    uint8_t ciphertext[16];
+    uint8_t decrypted[16];
+
+    
+
+    // Generate random key and IV
+    generateRandomPlaintext(key, sizeof(key));
+    generateRandomPlaintext(iv, sizeof(iv));
+
+    // Measure encryption time
+    unsigned long encryptionTime = 0;
+    unsigned long decryptionTime = 0;
+    for (int i = 0; i < NUM_RUNS; i++) {
+
+        //key generation + encryption
+        generateRandomPlaintext(plaintext, sizeof(plaintext));
+        unsigned long startTime = micros();
+        cipher -> setKey(key, sizeof(key));
+        cipher -> encryptBlock(ciphertext, plaintext);
+        unsigned long endTime = micros();
+        encryptionTime += (endTime - startTime);
+    
+        //decryption
+        startTime = micros();
+        cipher -> setKey(key, sizeof(key));
+        cipher -> decryptBlock(decrypted, ciphertext);
+        endTime = micros();
+        decryptionTime += (endTime - startTime);
+    }
+
+    //generating output
+    unsigned long averageEncryptionTime = encryptionTime / NUM_RUNS;
+    unsigned long averageDecryptionTime = decryptionTime / NUM_RUNS;
+    Serial.print("Average Encryption Time: ");
+    Serial.print(averageEncryptionTime);
+    Serial.println(" microseconds");
+
+    Serial.print("Average Decryption Time: ");
+    Serial.print(averageDecryptionTime);
+    Serial.println(" microseconds");
+    Serial.println();
+}
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println();
+    Serial.println("AES128");
+    runExperiment(&aes128);
+    Serial.println("AES192");
+    runExperiment(&aes192);
+    Serial.println("AES256");
+    runExperiment(&aes256);
+}
+
+void loop() {
+    // Empty loop
+}
